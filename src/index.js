c220dc6a71e935067c3deaa13ff7cf053013fd58
Purva Sheth <purvashet@gmail.com>
2021-06-07 00:16:53 +0530

    add chaching with hashing
    The hash is added in the file name which changes onlt if there is a change in file.
    Vendor files normally don't change and can be cached in browser. This reduces download time and increses performance

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const root = document.getElementById("root");
 
 root.innerHTML = `
 <img src="${Logo}" alt="build icon"/> 
-<h1>learning webpack today </h1>
+<h1>This is an webpack starter </h1>
 ${App({ name: "Purva" })}
 `;
