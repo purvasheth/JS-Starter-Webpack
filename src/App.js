7f78f7c3fa38946cf860ea218900dccb71fc9c9a
Purva Sheth <purvashet@gmail.com>
2021-06-06 21:29:13 +0530

    allow importing css file in javascript

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 const App = ({ name }) => {
 	return `
     <h2> Welcome, ${name} </h2>
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ const webpackConfig = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use:['style-loader', 'css-loader']
       }
     ]
   }
