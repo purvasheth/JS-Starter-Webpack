6fa8ee8f5c3bc26bfd434af18f69ae25606c4a4a
Purva Sheth <purvashet@gmail.com>
2021-06-06 21:41:11 +0530

    allow inporting images in JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from "./App";
+import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
 
 root.innerHTML = `
-<h1> learning webpack today </h1>
+<img src="${Logo}" alt="build icon"/> 
+<h1>learning webpack today </h1>
 ${App({ name: "Purva" })}
 `;
