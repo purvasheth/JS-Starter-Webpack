c5aee40c356956a5485c4f7da79c0190e0260059
Purva Sheth <purvashet@gmail.com>
2021-06-06 21:21:44 +0530

    add webpack and configure babel

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const webpackConfig = {
+	entry: path.resolve(__dirname, "src", "index.js"),
+
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist")
+	},
+
+  mode: "development",
+
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude: /(node_modules)/,
+        use:{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
+};
+
+module.exports = webpackConfig;
